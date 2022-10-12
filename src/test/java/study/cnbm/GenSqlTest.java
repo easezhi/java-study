8522396b594e1413fd03c70d04d3f568f0d4bd90
李智良 <easezhi@qq.com>
2022-10-12 17:40:23 +0800

    cnbm 生成字典insert语句

---
+++ src/test/java/study/cnbm/GenSqlTest.java
@@ -0,0 +1,42 @@
+package study.cnbm;
+
+import org.junit.Test;
+import study.cnbm.dict.DictDetail;
+
+import java.util.ArrayList;
+
+public class GenSqlTest {
+    @Test
+    public void testDictSql() {
+        String dictName = "";
+        String dictDesc = "";
+        var details = new ArrayList<DictDetail>();
+        details.add(new DictDetail("", ""));
+
+        var dictIdSql = """
+            (SELECT dict_id FROM public.sys_dict WHERE "name"='dictName')""".replaceAll("dictName", dictName);
+        var dictSql = """
+            DELETE FROM public.sys_dict_detail WHERE dict_id = dictIdSql;
+            DELETE FROM public.sys_dict WHERE "name" = 'dictName';
+
+            INSERT INTO public.sys_dict ("name", description, create_by, update_by, create_time, update_time, is_del) VALUES
+              ('dictName', 'dictDesc', 'admin_yw', 'admin_yw', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false);
+            INSERT INTO public.sys_dict_detail (dict_id, "label", value, dict_sort, create_by, update_by, create_time, update_time, is_del, enabled) VALUES"""
+            .replaceAll("dictIdSql", dictIdSql)
+            .replaceAll("dictName", dictName)
+            .replaceAll("dictDesc", dictDesc);
+        System.out.println(dictSql);
+        int total = details.size();
+        for (var i = 0; i < total; i++) {
+            var detail = details.get(i);
+            int sort = i + 1;
+            var detailSql = """
+                (dictIdSql, 'detailLabel', 'detailValue', detailSort, 'admin_yw', 'admin_yw', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false, true)"""
+                .replace("dictIdSql", dictIdSql)
+                .replace("detailLabel", detail.getLabel())
+                .replace("detailValue", detail.getValue())
+                .replace("detailSort", String.valueOf(sort));
+            System.out.println("  " + detailSql + (i < (total - 1) ? "," : ";"));
+        }
+    }
+}
--- src/test/java/study/cnbm/dict/DictDetail.java
+++ src/test/java/study/cnbm/dict/DictDetail.java
@@ -4,16 +4,23 @@ import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.annotation.JSONField;
 import easezhi.study.io.FileUtil;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Data
+@NoArgsConstructor
 public class DictDetail {
     String label;
 
     String value;
+
+    public DictDetail(String value, String label) {
+        this.value = value;
+        this.label = label;
+    }
 
     public List<DictDetail> getDictFromJson() {
         var file = "test\\resources\\cnbm\\dict_detail.json";
