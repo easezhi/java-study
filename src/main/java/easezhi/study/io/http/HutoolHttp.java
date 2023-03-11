821284770ba5817f0027b7e616739eecf7a8641d
李智良 <easezhi@qq.com>
2023-03-12 01:04:40 +0800

    hutool http

--- src/main/java/easezhi/study/io/http/HutoolHttp.java
+++ src/main/java/easezhi/study/io/http/HutoolHttp.java
@@ -1,14 +1,25 @@
 package easezhi.study.io.http;
 
 import cn.hutool.http.HttpRequest;
+import com.alibaba.fastjson2.JSON;
 
 public class HutoolHttp {
 
-    public static String postJson(String url, Object body) {
-        return null;
+    public static String postJson(String url, Object data) {
+        String body;
+        if (data instanceof String) {
+            body = (String) data;
+        } else {
+            body = JSON.toJSONString(data);
+        }
+        return HttpRequest.post(url)
+            .body(body)
+            .execute()
+            .body();
     }
 
     public static <E> E postJson(String url, Object body, Class<E> clazz) {
-        return null;
+        var rst = postJson(url, body);
+        return JSON.parseObject(rst, clazz);
     }
 }
